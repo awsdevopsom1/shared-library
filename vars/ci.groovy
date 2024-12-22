1a0c76b08194ddfa5280142edc62767d49f1775d
om <awsdevopsom@gmail.com>
2024-12-22 22:56:49 +0530

    op

---
+++ vars/ci.groovy
@@ -0,0 +1,30 @@
+def call () {
+pipeline {
+    agent {
+        node { label 'workstation1'}
+    }
+    stages {
+        stage('compile'){
+            steps{
+                echo 'compile'
+            }
+        }
+        stage('test cases'){
+            steps{
+                echo 'compile'
+            }
+        }
+        stage('Build'){
+            steps{
+                echo 'compile'
+            }
+        }
+        stage('Release'){
+            steps{
+                echo 'compile'
+            }
+        }
+    }
+}  
+
+}
