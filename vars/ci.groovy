57633e195f90f064539704d07ecf2317da17d72a
om <awsdevopsom@gmail.com>
2024-12-24 19:32:17 +0530

    om

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,10 +1,10 @@
 def call () {
- node {'workstation1'} {
-       stage('compile'){}
-       stage('test cases'){}
-       stage('integration test cases')
-       stage('Build'){}
-       stage('Release_App'){}
+  node {'workstation1'} {
+    stage('compile'){}
+    stage('test cases'){}
+    stage('integration test cases')
+    stage('Build'){}
+    stage('Release_App'){}
     }
 
 }  
