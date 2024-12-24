4b0a78a01ce0c51ea5cccca3c40496758327b463
om <awsdevopsom@gmail.com>
2024-12-24 21:19:15 +0530

    up

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,11 +1,17 @@
 def call () {
   node ('workstation1') {
     sh 'env'
+    if(BRANCH_NAME == "main")
+    stage('code checkout'){}
     stage('compile'){}
-    stage('test cases'){}
-    stage('integration test cases')
-    stage('Build'){}
-    stage('Release_App'){}
+     stage('Build'){}
+
+
+
+    // stage('test cases'){}
+    // stage('integration test cases')
+   
+    // stage('Release_App'){}
     }
 
 }  
