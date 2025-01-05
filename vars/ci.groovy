4a99001eac463480b1c5425e18ef55c1c21a6369
om <awsdevopsom@gmail.com>
2025-01-05 23:19:35 +0530

    update

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,6 +6,17 @@ def call (){
          stage('codecheckout'){}
          stage('codecompile'){}
          stage('codeBuild'){}
+      } else if(env.Branch_name ==~ "PR.*"){
+         stage('codecheckout'){}
+         stage('codecompile'){}
+         stage('testcases') {}
+         stage('integrationtestcases'){}
+
+      } else if (env.TAG_name ==~ ".*"){
+         stage('codecheckout'){}
+         stage('codecompile'){}
+         stage('build'){}
+         stage('release'){}
       }
 
       else {
