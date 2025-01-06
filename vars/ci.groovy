1f3fa3eea4be41245fc9a3b0e271192e2fd28020
om <awsdevopsom@gmail.com>
2025-01-06 15:20:07 +0530

    om

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -14,7 +14,7 @@ def call (){
          stage('codecheckout'){
             checkout scmGit(branches: [[name: "${env.branchName}"]],
             extensions: [],
-            userRemoteConfigs: [[url: 'https://github.com/awsdevopsom1/import-backend.git']])
+            userRemoteConfigs: [[url: 'https://github.com/awsdevopsom1/${repo_name}.git']])
          }
          sh 'ls'
          sh 'cat Jenkinsfile'
